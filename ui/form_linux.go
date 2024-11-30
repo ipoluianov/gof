a2daad32cfc446d1dc1cab239f292aacbc66ac70
ip <ivan.poluianov@gmail.com>
2024-11-30 10:03:44 +0100

    Update form_linux.go

--- ui/form_linux.go
+++ ui/form_linux.go
@@ -4,6 +4,7 @@ package ui
 #cgo LDFLAGS: -lX11
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
+#include <X11/Xatom.h>
 #include <stdlib.h>
 
 void DestroyXImage(XImage *image) {
@@ -15,13 +16,34 @@ import "C"
 import (
 	"bytes"
 	_ "embed"
+	"fmt"
 	"image"
 	"image/png"
+	"time"
 	"unsafe"
 )
 
+var posX C.uint
+var posY C.uint
 var width C.uint
 var height C.uint
+
+func resizeWindow(display *C.Display, window C.Window, width, height int) {
+	C.XResizeWindow(display, window, C.uint(width), C.uint(height))
+	fmt.Printf("Window resized to %dx%d\n", width, height)
+}
+
+func moveWindow(display *C.Display, window C.Window, x, y int) {
+	C.XMoveWindow(display, window, C.int(x), C.int(y))
+	fmt.Printf("Window moved to (%d, %d)\n", x, y)
+}
+
+func setWindowTitle(display *C.Display, window C.Window, title string) {
+	cTitle := C.CString(title)
+	defer C.free(unsafe.Pointer(cTitle))
+	C.XStoreName(display, window, cTitle)
+	fmt.Printf("Window title set to: %s\n", title)
+}
 
 func Run() {
 	display := C.XOpenDisplay(nil)
@@ -42,27 +64,89 @@ func Run() {
 		C.XWhitePixel(display, screen),
 	)
 
-	C.XSelectInput(display, window, C.ExposureMask|C.KeyPressMask|C.StructureNotifyMask)
+	C.XSelectInput(display, window, C.ExposureMask|C.PropertyChangeMask|C.ResizeRedirectMask|C.StructureNotifyMask|C.KeyPressMask|C.StructureNotifyMask|C.KeyReleaseMask|C.EnterWindowMask|C.LeaveWindowMask|C.ButtonPressMask|C.ButtonReleaseMask|C.PointerMotionMask)
 
 	C.XMapWindow(display, window)
 
-	for {
-		var event C.XEvent
-		C.XNextEvent(display, &event)
+	ticker := time.NewTicker(10 * time.Millisecond)
+	defer ticker.Stop()
 
-		switch eventType(event) {
-		case C.Expose:
-			drawBlue(display, window, screen)
-			img, err := loadImageFromEmbed()
-			if err == nil {
-				drawImageRGBA(display, window, img)
+	for {
+		for C.XPending(display) > 0 {
+			var event C.XEvent
+			C.XNextEvent(display, &event)
+
+			switch eventType(event) {
+
+			case C.Expose:
+				drawBlue(display, window, screen)
+				img, err := loadImageFromEmbed()
+				if err == nil {
+					drawImageRGBA(display, window, img)
+				}
+			case C.MapNotify:
+				mapEvent := (*C.XMapEvent)(unsafe.Pointer(&event))
+				fmt.Printf("Window became visible. Window ID: %d\n", mapEvent.window)
+
+			case C.UnmapNotify:
+				unmapEvent := (*C.XUnmapEvent)(unsafe.Pointer(&event))
+				fmt.Printf("Window was hidden. Window ID: %d\n", unmapEvent.window)
+			case C.DestroyNotify:
+				destroyEvent := (*C.XDestroyWindowEvent)(unsafe.Pointer(&event))
+				fmt.Printf("Window was destroyed. Window ID: %d\n", destroyEvent.window)
+
+			case C.ReparentNotify:
+				reparentEvent := (*C.XReparentEvent)(unsafe.Pointer(&event))
+				fmt.Printf("Window changed parent. Window ID: %d, New Parent ID: %d\n", reparentEvent.window, reparentEvent.parent)
+			case C.ResizeRequest:
+				resizeEvent := (*C.XResizeRequestEvent)(unsafe.Pointer(&event))
+				fmt.Printf("Resize request received: Width=%d, Height=%d\n", resizeEvent.width, resizeEvent.height)
+
+			case C.ConfigureNotify:
+				configureEvent := (*C.XConfigureEvent)(unsafe.Pointer(&event))
+				posX = C.uint(configureEvent.x)
+				posY = C.uint(configureEvent.y)
+				width = C.uint(configureEvent.width)
+				height = C.uint(configureEvent.height)
+				fmt.Println("Configure:", posX, posY, width, height)
+
+			case C.KeyPress:
+				keyEvent := (*C.XKeyEvent)(unsafe.Pointer(&event))
+				keySym := C.XLookupKeysym((*C.XKeyEvent)(unsafe.Pointer(&event)), 0)
+				fmt.Printf("Key pressed: KeySym = %d, KeyCode = %d\n", keySym, keyEvent.keycode)
+
+				//resizeWindow(display, window, 600, 200)
+				//moveWindow(display, window, 100, 100)
+				//setWindowTitle(display, window, "HELLO")
+			case C.KeyRelease:
+				keyEvent := (*C.XKeyEvent)(unsafe.Pointer(&event))
+				keySym := C.XLookupKeysym(keyEvent, 0)
+				fmt.Printf("Key released: KeySym = %d, KeyCode = %d\n", keySym, keyEvent.keycode)
+			case C.EnterNotify:
+				enterEvent := (*C.XCrossingEvent)(unsafe.Pointer(&event))
+				fmt.Printf("Cursor entered window at (%d, %d)\n", enterEvent.x, enterEvent.y)
+
+			case C.LeaveNotify:
+				leaveEvent := (*C.XCrossingEvent)(unsafe.Pointer(&event))
+				fmt.Printf("Cursor left window at (%d, %d)\n", leaveEvent.x, leaveEvent.y)
+			case C.MotionNotify:
+				motionEvent := (*C.XMotionEvent)(unsafe.Pointer(&event))
+				fmt.Printf("Mouse moved to (%d, %d)\n", motionEvent.x, motionEvent.y)
+
+			case C.ButtonPress:
+				buttonEvent := (*C.XButtonEvent)(unsafe.Pointer(&event))
+				fmt.Printf("Mouse button %d pressed at (%d, %d)\n", buttonEvent.button, buttonEvent.x, buttonEvent.y)
+
+			case C.ButtonRelease:
+				buttonEvent := (*C.XButtonEvent)(unsafe.Pointer(&event))
+				fmt.Printf("Mouse button %d released at (%d, %d)\n", buttonEvent.button, buttonEvent.x, buttonEvent.y)
 			}
-		case C.ConfigureNotify:
-			configureEvent := (*C.XConfigureEvent)(unsafe.Pointer(&event))
-			width = C.uint(configureEvent.width)
-			height = C.uint(configureEvent.height)
-		case C.KeyPress:
-			return
+		}
+
+		select {
+		case <-ticker.C:
+			//fmt.Println("Timer event: 10ms tick")
+		default:
 		}
 	}
 }
