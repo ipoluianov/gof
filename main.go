1ac1f1c14c7a79461e92bea1871f82a0fc548f3e
ip <ivan.poluianov@gmail.com>
2024-12-22 19:22:13 +0100

    mac

--- main.go
+++ main.go
@@ -8,8 +8,43 @@ package main
 #import <Foundation/Foundation.h>
 #import <Cocoa/Cocoa.h>
 
+// Кастомный класс NSView для отрисовки изображения
+@interface CustomImageView : NSView
+@property (assign) CGImageRef image;
+@end
+
+@implementation CustomImageView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+
+    if (self.image) {
+        CGContextRef context = [[NSGraphicsContext currentContext] CGContext];
+        CGRect rect = NSRectToCGRect([self bounds]);
+        CGContextDrawImage(context, rect, self.image);
+    }
+}
+
+- (void)setImage:(CGImageRef)newImage {
+    if (_image) {
+        CGImageRelease(_image); // Освобождаем старое изображение
+    }
+    _image = CGImageRetain(newImage); // Сохраняем новое изображение
+    [self setNeedsDisplay:YES]; // Запрашиваем перерисовку
+}
+
+- (void)dealloc {
+    if (_image) {
+        CGImageRelease(_image); // Освобождаем изображение при удалении
+    }
+    [super dealloc];
+}
+
+@end
+
 @interface AppDelegate : NSObject <NSApplicationDelegate, NSWindowDelegate>
 @property (assign) NSWindow *window;
+@property (assign) CustomImageView *imageView; // Используем кастомный NSView
 @end
 
 @implementation AppDelegate
@@ -26,12 +61,48 @@ package main
     [self.window setDelegate:self];
     [self.window makeKeyAndOrderFront:nil];
 
+	 // Создаем CustomImageView
+    self.imageView = [[CustomImageView alloc] initWithFrame:[[self.window contentView] bounds]];
+    [[self.window contentView] addSubview:self.imageView];
+
     [NSApp activateIgnoringOtherApps:YES];
+}
+
+- (void)setImageWithBytes:(void *)bytes width:(int)width height:(int)height {
+    NSLog(@"Setting image with width: %d, height: %d", width, height);
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(
+        bytes,
+        width,
+        height,
+        8, // 8 бит на компонент
+        width * 4, // 4 байта на пиксель (RGBA)
+        colorSpace,
+        kCGImageAlphaPremultipliedLast | kCGBitmapByteOrderDefault
+    );
+
+    if (!context) {
+        NSLog(@"Failed to create CGContext");
+    }
+
+    CGImageRef image = CGBitmapContextCreateImage(context);
+    if (!image) {
+        NSLog(@"Failed to create CGImage");
+    } else {
+        NSLog(@"Image created successfully");
+    }
+
+    [self.imageView setImage:image];
+
+    CGImageRelease(image);
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpace);
 }
 
 - (void)windowDidResize:(NSNotification *)notification {
     NSRect frame = [self.window frame];
-    NSLog(@"Window resized to: %.0fx%.0f", frame.size.width, frame.size.height);
+    [self.imageView setFrame:[[self.window contentView] bounds]]; // Подгоняем под размер окна
+    [self.imageView setNeedsDisplay:YES]; // Перерисовываем
 }
 
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
@@ -41,13 +112,21 @@ package main
 - (void)windowWillClose:(NSNotification *)notification {
     [NSApp terminate:nil];
 }
+
+
 @end
+
+AppDelegate *delegate;
 
 void RunApplication() {
     [NSApplication sharedApplication];
-    AppDelegate *delegate = [[AppDelegate alloc] init];
+    delegate = [[AppDelegate alloc] init];
     [NSApp setDelegate:delegate];
     [NSApp run];
+}
+
+void SetImage(void *bytes, int width, int height) {
+    [delegate setImageWithBytes:bytes width:width height:height];
 }
 
 */
@@ -55,6 +134,7 @@ import "C"
 import (
 	"image"
 	"image/color"
+	"unsafe"
 )
 
 func generateRGBA(width, height int) []byte {
@@ -73,5 +153,8 @@ func generateRGBA(width, height int) []byte {
 }
 
 func main() {
+	const width, height = 800, 600
+	rgbaBytes := generateRGBA(width, height)
+	C.SetImage(unsafe.Pointer(&rgbaBytes[0]), C.int(width), C.int(height))
 	C.RunApplication()
 }
